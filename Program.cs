ccdfe339a06d7dfb6e907f47956d25320fb67ad2
Domn Werner <domn.werner@gmail.com>
2020-07-05 13:11:36 -0700

    Detecting speech in real time and output to console.

--- Program.cs
+++ Program.cs
@@ -1,53 +1,15 @@
-﻿//
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
-//
-
-using System;
-using System.Threading.Tasks;
-using Microsoft.CognitiveServices.Speech;
-using Microsoft.CognitiveServices.Speech.Audio;
+﻿using System.Threading.Tasks;
+using CliFx;
 
 namespace HelloWorld
 {
     public static class Program
     {
-        static async Task Main(string[] args)
-        {
-            await RecognizeSpeechAsync(args[0]);
-        }
-
-        static async Task RecognizeSpeechAsync(string inputFile)
-        {
-            var apiKey = Environment.GetEnvironmentVariable("AZURE_COG_SVCS_API_KEY");
-            var config = SpeechConfig.FromSubscription(apiKey, "westus2");
-
-            using var audioInput = AudioConfig.FromWavFileInput(inputFile);
-            using var recognizer = new SpeechRecognizer(config, audioInput);
-            
-                Console.WriteLine("Recognizing first result...");
-                var result = await recognizer.RecognizeOnceAsync();
-
-                switch (result.Reason)
-                {
-                    case ResultReason.RecognizedSpeech:
-                        Console.WriteLine($"We recognized: {result.Text}");
-                        break;
-                    case ResultReason.NoMatch:
-                        Console.WriteLine($"NOMATCH: Speech could not be recognized.");
-                        break;
-                    case ResultReason.Canceled:
-                        var cancellation = CancellationDetails.FromResult(result);
-                        Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
-                
-                        if (cancellation.Reason == CancellationReason.Error)
-                        {
-                            Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
-                            Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
-                            Console.WriteLine($"CANCELED: Did you update the subscription info?");
-                        }
-                        break;                
-            }
-        }
+        public static async Task<int> Main() =>
+            await new CliApplicationBuilder()
+                .AddCommandsFromThisAssembly()
+                .UseExecutableName("s2t")
+                .Build()
+                .RunAsync();
     }
 }
---
+++ TranscribeCommand.cs
@@ -0,0 +1,81 @@
+﻿using CliFx;
+using CliFx.Attributes;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Transcriber
+{
+    [Command]
+    public class TranscribeCommand : ICommand
+    {
+        [CommandOption("key", 'k', Description = "Azure Cognitive Services API Key.",
+            EnvironmentVariableName = "AZURE_COG_SVCS_API_KEY", IsRequired = false)]
+        public string ApiKey { get; set; }
+
+        [CommandOption("in", 'i', Description = "Input file (.wav).", IsRequired = true)]
+        public FileInfo InputFile { get; set; }
+
+        [CommandOption("out", 'o', Description = "Output file (.txt).", IsRequired = false)]
+        public FileInfo OutputFile { get; set; } = new FileInfo(Path.Combine(Environment.CurrentDirectory, "out.txt"));
+
+        [CommandOption("region", 'r', Description = "Azure region.", IsRequired = false)]
+        public string Region { get; set; } = "westus2";
+
+        public async ValueTask ExecuteAsync(IConsole console)
+        {
+            var config = SpeechConfig.FromSubscription(ApiKey, Region);
+
+            using var audioInput = AudioConfig.FromWavFileInput(InputFile.FullName);
+            using var recognizer = new SpeechRecognizer(config, audioInput);
+
+            var stopRecognition = new TaskCompletionSource<int>();
+            Console.Clear();
+            recognizer.Recognizing += (o, e) =>
+            {                
+                Console.SetCursorPosition(0, 0);
+                Console.Write(e.Result.Text);
+            };
+
+            recognizer.Recognized += OnSpeechRecognized;            
+
+            await recognizer.StartContinuousRecognitionAsync().ConfigureAwait(false);
+
+            Task.WaitAny(new[] { stopRecognition.Task });
+
+            await recognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
+
+            void OnSpeechRecognized(object sender, SpeechRecognitionEventArgs e)
+            {
+                var result = e.Result;
+
+                switch (result.Reason)
+                {
+                    case ResultReason.RecognizedSpeech:
+                        console.Output.Write($"We recognized: {result.Text}");
+                        break;
+                    case ResultReason.NoMatch:
+                        console.Output.WriteLine($"NOMATCH: Speech could not be recognized.");
+                        break;
+                    case ResultReason.Canceled:
+                        var cancellation = CancellationDetails.FromResult(result);
+                        console.Output.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                        if (cancellation.Reason == CancellationReason.Error)
+                        {
+                            console.Output.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                            console.Output.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                            console.Output.WriteLine($"CANCELED: Did you update the subscription info?");
+                        }
+                        stopRecognition.TrySetResult(0);
+                        break;
+                    default:
+                        console.Output.WriteLine("ERROR: An unknown problem ocurred.");
+                        break;
+                }
+            }
+        }
+    }
+}
